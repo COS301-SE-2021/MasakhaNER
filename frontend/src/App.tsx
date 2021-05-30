0e9a81b81bc5c0929b37e162abd5d49c2199c7cc
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2021-05-30 21:59:55 +0200

    commented out useEffect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,20 +3,19 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [time, setTime] = useState(0);
+  // const [time, setTime] = useState(0);
 
-  useEffect(() => {
-    fetch("/index")
-      .then((res) => res.json())
-      .then((data) => {
-        setTime(data.time)
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("/index")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setTime(data.time)
+  //     });
+  // }, []);
 
   return (
     <div className="App">
       <h1>StopDaCap : MasakhaNER</h1>
-      <h3>{time}</h3>
     </div>
   );
 }
