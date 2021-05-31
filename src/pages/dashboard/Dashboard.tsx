0519aafca5041644cf478748d3c734c3805188a3
siphoxnkosi <u19107383@tuks.co.za>
2021-05-31 21:57:24 +0200

    Created Footer Component - Sean Nkosi

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from '../../components/nav/Nav'
+import Footer from '../../components/Footer/Footer'
 
 function Dashboard() {
     return (
         <div>
             <Nav />
+            <Footer />
         </div>
     )
 }
