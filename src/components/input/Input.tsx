26f9e4a2bd59ef695bc61994b2fc6ca701def2fe
Siphesihle05 <u19078286@tuks.co.za>
2021-06-01 03:07:07 +0200

    Add input section

---
+++ src/components/input/Input.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Input = () => {
+    return (
+        <div className = 'input'>
+            <div id = 'top'></div>
+            <div id = 'input'></div>
+            <div id = 'output'></div>
+        </div>
+    )
+}
