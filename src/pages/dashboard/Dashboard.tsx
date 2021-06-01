c153858adac3736ada7d6e3b5e38afaff5f0ec99
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2021-06-01 18:17:21 +0200

    ediited folder names

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { Input } from '../../components/input/Input'
-import Nav from '../../components/nav/Nav'
-import Footer from '../../components/Footer/Footer'
+import React from "react";
+import { Input } from "../../components/Input/Input";
+import Nav from "../../components/Nav/Nav";
+import Footer from "../../components/Footer/Footer";
 
 function Dashboard() {
-    return (
-        <div>
-            <Nav />
-            <Input />
-            <Footer />
-        </div>
-    )
+  return (
+    <div>
+      <Nav />
+      <Input />
+      <Footer />
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
