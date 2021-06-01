9866a0e3325820e84493bf2cb1a9c4aa606bcba4
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2021-06-01 21:16:22 +0200

    Created input endpoint and NER model mock functionality

--- api/main.py
+++ api/main.py
@@ -1,12 +1,53 @@
 from datetime import datetime
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
+
+compareList = [
+        ['michael','person'],
+        ['johny','person'],
+        ['erick','person'],
+        ['kanye','person'],
+        ['london','location'],
+        ['johannesburg','location'],
+        ['cape town','location'],
+        ['tokyo','location'],
+        ['janauary','date'],
+        ['febraury','date'],
+        ['today','date'],
+        ['monday','date'],
+        ['yesterday','date'],
+    ]
 
 @app.route('/index')
 def get_current_time():
     now = datetime.now()
     return {'time': now}
+
+def annotate(model_output):
+    newlist = []
+    i=0;
+    for x in model_output:
+        for y in compareList:
+            if x.lower() == y[0]:
+                print(x)
+                newlist.append([{"name":x, "entity": y[1]}])
+                i=0
+                break
+            if i == len(compareList):
+                i=0;
+    return newlist
+
+@app.route('/input', methods=["POST"])
+def model_feedback():
+    model_output = str(request.json["input"]).split()
+
+    
+    
+    annotatedlist = annotate(model_output)
+
+    return {'output': annotatedlist}
 
 if __name__ == "__main__":
     app.run(debug=True)
