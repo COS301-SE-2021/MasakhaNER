453da51d11dd0638ef9b7e11054db7f95df2846f
Siphesihle05 <u19078286@tuks.co.za>
2021-06-02 00:14:27 +0200

    Created Login.css

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Input } from "../../components/input/Input";
 import Nav from "../../components/nav/Nav";
 import Footer from "../../components/Footer/Footer";
+import Login from "../../components/login/Login";
 
 function Dashboard() {
   return (
