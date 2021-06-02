d5d741d78772f821ad56ecb55537fb0a10527f2a
siphoxnkosi <u19107383@tuks.co.za>
2021-06-03 01:17:48 +0200

    Coded interface

--- src/pages/verify/verifyAccount.tsx
+++ src/pages/verify/verifyAccount.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function verifyAccount() {
     return (
         <div>
-            
+            <h3>Enter Confirmation Code</h3>
+            <p>Enter the confirmation code we sent to your email.</p>
+            <input type="text" placeholder="Confirmation Code"/>
+            <button type="submit">Next</button>
         </div>
     )
 }
