b80f229ac09f63ea4145cf80450e68efa0ca1683
Siphesihle05 <u19078286@tuks.co.za>
2021-06-02 18:38:18 +0200

    Added Google and facebook login options

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,29 +1,58 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { FaLock } from "react-icons/fa";
-import { FaMailBulk } from "react-icons/fa";
-import './Login.css'
+import GoogleLogin from 'react-google-login'
+import FacebookLogin from 'react-facebook-login'
+import './Login.css';
+import React, { useState } from 'react';
+import { validEmail, validPassword } from './Regex';
+import {BrowserRouter} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
-export default function Login() {
+const responseGoogle = (response: any) =>{
+    console.log(response);
+}
+const responseFacebook = (response: any) => {
+    console.log(response);
+}
+
+export default function Login(this: any) {
+    const [email, setEmail] = useState('');
+    const [err, setEmailErr] = useState(false);
+    const validate = () => {
+        console.log(email);
+        if (!validEmail.test(email)) {
+            setEmailErr(true)
+        }
+     };
     return (
         <div className = 'login'>
             <div className = 'loginTop'>
                 <h2>Log In</h2>
             </div>
-            <form className = 'loginForm'action="">
-                <div>
-                    <label htmlFor="email">Email:  </label>
-                    <input type="text" id = 'email' placeholder = "Email" required /><br/>
-                </div><br/>
-                <div>
-                    <label htmlFor="pass">Password:  </label>
-                    <input type="password" id = 'pass' placeholder = 'Password' required/><br/>
-                </div><br/>
-                <input type = 'submit' className = 'btn btn-success' value = 'Login' /><br/>
+            <div className = "loginForm">
+                <input type="email" id ='email' placeholder = "Email" value={email} onChange={(e) => setEmail(e.target.value)} required /><br/>
+                <input type="password" placeholder = 'Password' required/><br/>
+                <button  className = 'btn btn-success'  onClick={validate} >Login</button><br/>
                 <a href="#" className = 'btn btn-primary'>Signup</a>
-            </form> 
+            </div>
+            <GoogleLogin 
+                clientId = "824866690096-4rqi2a1n6bvj9sfstjcbv999i9pi69i3.apps.googleusercontent.com"
+                buttonText = 'Login'
+                onSuccess = {responseGoogle}
+                onFailure = {responseGoogle}
+                cookiePolicy = {'single_host_origin'}
+            />
+            <FacebookLogin
+                appId="2951110285136034"
+                autoLoad={true}
+                fields="name,email,picture"
+                callback={responseFacebook} 
+                icon="fa-facebook"
+            />
+            {err && <p>Envalid email</p> }
             <div className = 'forgot'>
                 <a href="# ">forgot password?</a>
             </div>
         </div>
+        
     )
 }
