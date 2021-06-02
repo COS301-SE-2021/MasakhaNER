a50d62bc759c87909fc5aff5b510c467a3d2ee1f
siphoxnkosi <u19107383@tuks.co.za>
2021-06-03 01:39:20 +0200

    Updated CSS

--- src/pages/verify/verifyAccount.tsx
+++ src/pages/verify/verifyAccount.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Container } from "react-bootstrap";
 
-function verifyAccount() {
+function VerifyAccount() {
     return (
-        <div>
+    <div className="signup-form">
+        <form>
             <h3>Enter Confirmation Code</h3>
             <p>Enter the confirmation code we sent to your email.</p>
-            <input type="text" placeholder="Confirmation Code" className="form-control"/>
+            <div className="form-group">
+                <input type="text" placeholder="Confirmation Code" className="form-control"/>
+            </div>
             <button type="submit" className="btn btn-primary">Next</button>
-        </div>
+        </form>
+    </div>
     )
 }
 
-export default verifyAccount
+export default VerifyAccount
