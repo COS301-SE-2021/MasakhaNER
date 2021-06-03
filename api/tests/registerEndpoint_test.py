fbc675d16fafb3029f5734622ae1b37f3bda0334
Khotso-Bore <82458587+Khotso-Bore@users.noreply.github.com>
2021-06-04 00:36:43 +0200

    edited register fie

--- api/tests/registerEndpoint_test.py
+++ api/tests/registerEndpoint_test.py
@@ -6,21 +6,24 @@ class Test(unittest.TestCase):
     ENDPOINT_URL = "http://127.0.0.1:5000/register"
     
     INPUT = {
-        "input": "Kanye in London"
+        "firstname": "man",
+        "lastname": "manson",
+        "email": "m@gmail.com",
+        "password": "1234"
     }
     
     def test_endpoint(self):
-        print("week")
+        
         r = requests.post(self.ENDPOINT_URL,json=self.INPUT)
         data = json.loads(r.content)
-
-        result1 = data['output'][0]
-        result2 = data['output'][1]
+        print(data)
+        # result1 = data['output'][0]
+        # result2 = data['output'][1]
 
         # print(result1)
         # print(result2)
-        self.assertEqual(200, r.status_code)
-        self.assertEqual(result1['entity'],'PERSON')
-        self.assertEqual(result1['name'],'Kanye')
-        self.assertEqual(result2['entity'],'LOCATION')
-        self.assertEqual(result2['name'],'London')
+        # self.assertEqual(200, r.status_code)
+        # self.assertEqual(result1['entity'],'PERSON')
+        # self.assertEqual(result1['name'],'Kanye')
+        # self.assertEqual(result2['entity'],'LOCATION')
+        # self.assertEqual(result2['name'],'London')
