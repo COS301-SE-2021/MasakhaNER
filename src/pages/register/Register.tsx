fce30a68ec95c1cc6d85790abb352163ae97e08e
siphoxnkosi <u19107383@tuks.co.za>
2021-06-13 01:10:46 +0200

    removed unused imports

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect, useRef, Ref } from "react";
+import React, { useState, useEffect} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Register.css";
 import ReCAPTCHA from "react-google-recaptcha";
-import { useForm } from 'react-hook-form';
 
 
 function Register() {
