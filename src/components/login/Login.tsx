5c82b9099d8a35fac7934df6f98f123508be631a
Siphesihle05 <u19078286@tuks.co.za>
2021-06-19 01:20:46 +0200

    Added Login authentication

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -2,10 +2,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import GoogleLogin from "react-google-login";
 import FacebookLogin from "react-facebook-login";
 import "./Login.css";
-import React, { useState, useEffect } from "react";
-import { validEmail } from "./Regex";
+import { useState, useEffect } from "react";
+import { validEmail, validPassword } from "./Regex";
 import { useHistory } from "react-router-dom";
-import {FaLinkedin} from "react-icons/fa"
+import {FaLinkedin} from "react-icons/fa";
+import axios from "axios";
 
 
 let history;
@@ -17,56 +18,73 @@ const responseFacebook = (response: any) => {
   console.log(response);
   history.push("/Dashboard");
 };
+const responseFailerGoogle = (reoponse: any) =>{
+  console.log("failed");
+  history.push("/");
+}
 
-
-export default function Login(this: any) {
+export default function Login(setToken: any) {
   history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [err, setEmailErr] = useState(false);
+  const [passErr, setPasswordErr] = useState(false);
   const [clicked, setClicked] = useState(false);
-
-  const validate = () => {
-    if (!validEmail.test(email)) {
-      setEmailErr(true);
-    } else {
-      setClicked(!clicked);
-      history.push("/Dashboard");
-    }
-  };
+  const [state, setUser] = useState("");
 
   const register = () =>{
     history.push("/register");
   }
 
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ email: email, password: password }),
-  };
+  // const options = {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({ email: email, password: password }),
+  // };
 
-  useEffect(() => {
-    if (err) {
-      fetch("/login", options)
-        .then((res) => res.json())
-        .then((data) => {
-          console.log("data: ", data);
-        })
-        .catch((err) => console.log(err));
-    }
-  }, [clicked]);
+  // useEffect(() => {
+  //   if (err) {
+  //     fetch("/login", options)
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         console.log("data: ", data);
+  //       })
+  //       .catch((err) => console.log(err));
+  //   }
+  // }, [clicked]);
 
-  const callbackLinkedIn = (error: any, code: any, redirectUri: any) => {
-    if (error) {
-      // signin failed
-    } else {
-      history.push("/Dashboard");
-      // Obtain authorization token from linkedin api
-      // see https://developer.linkedin.com/docs/oauth2 for more info
+  const handleLogin = async () =>{
+    const opts ={
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "email": email,
+        "password": password
+      })
     }
-  };
+    try{
+      const resp = await fetch("/login", opts);
+      console.log(resp);
+      if(resp.status === 200){
+        const data = await resp.json();
+        console.log(data);
+        localStorage.setItem("token", data.token);
+        history.push("/Dashboard");
+      }
+      else{
+        alert("Incorrect login or user does not exists!!!");
+        history.push("/");
+      }
+    }
+    catch(error){
+      console.log("there is an error", error);
+      history.push("/");
+    }
+  }
 
   return (
     <div className="login-body">
@@ -76,22 +94,33 @@ export default function Login(this: any) {
 
       </div>
 
-      <div className="login">
+      <div  className="login">
         <div className="loginTop">
           <h2>LOGIN</h2>
         </div>
         <div className="loginForm">
           <label htmlFor="email">Email address:</label>
-          <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+          <input 
+            type="email" 
+            id="email" 
+            name="email" 
+            value={email} onChange={(e) => setEmail(e.target.value)} 
+            required 
+          />
           <br />
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" required />
+          <input 
+            type="password" 
+            name="password" 
+            value={password} onChange={(e) => setPassword(e.target.value)} 
+            required 
+          />
           <br />
 
           <div className = "login-button">
             <span>
               <a href="# ">forgot password?</a>
-              <button className="btn btn-dark" onClick={validate}>Login</button>
+              <button onClick = {handleLogin}  className="btn btn-dark">Login</button>
             </span>
             <span>
               <br/>
@@ -106,12 +135,12 @@ export default function Login(this: any) {
             clientId="824866690096-4rqi2a1n6bvj9sfstjcbv999i9pi69i3.apps.googleusercontent.com"
             buttonText=""
             onSuccess={responseGoogle}
-            onFailure={responseGoogle}
+            onFailure={responseFailerGoogle}
             cookiePolicy={"single_host_origin"}
           />
           <FacebookLogin
             appId="2951110285136034"
-            autoLoad={true}
+            autoLoad={false}
             fields="name,email,picture"
             callback={responseFacebook}
             cssClass = "btn btn-primary"
@@ -128,3 +157,7 @@ export default function Login(this: any) {
     </div>
   );
 }
+function then() {
+  throw new Error("Function not implemented.");
+}
+
