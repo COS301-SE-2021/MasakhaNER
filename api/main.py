f5143a2e7763869ac3ef7374550115d78293b03c
u19081082 <u19081082@tuks.co.za>
2021-06-21 15:06:26 +0200

    update email

--- api/main.py
+++ api/main.py
@@ -284,6 +284,8 @@ def update_password(user):
     if(db != None):
         user_email = str(request.json["email"])
         user_password = str(request.json["password"])
+        if(db.update( user_email, user_password)):
+            return {'response':'updated'}
 
 if __name__ == "__main__":
     app.run(debug=True)
