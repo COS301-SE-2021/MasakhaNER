d0ecc3846de645917ec0241d257eff2c36b67d92
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2021-08-05 09:05:26 +0200

    Update Output.tsx

--- src/components/Output/Output.tsx
+++ src/components/Output/Output.tsx
@@ -3,26 +3,54 @@ import { useState, useEffect } from "react";
 import styled from "styled-components";
 
 const Text = styled.div`
-  #LOC {
-    background-color:orange
-  }
-  #DATE{
-    background-color:royalblue
-  }
-  #PER{
-    background-color:greenyellow
-  }
-  #ORG{
-    background-color:hotpink
-  }`
-;
+  line-height: 33px;
+  color: #5f5f5f;
 
+  span {
+    border-radius: 8px;
+    padding: 3px;
+  }
+
+  #LOC {
+    background-color: #ffcb69;
+    color: #634000;
+
+    #tag {
+      width: 30px;
+      color: white;
+    }
+  }
+  #DATE {
+    background-color: #7793e9;
+    color: #092781;
+    #tag {
+      width: 30px;
+      color: white;
+    }
+  }
+  #PER {
+    background-color: #c1f177;
+    color: #538606;
+    #tag {
+      width: 30px;
+      color: white;
+    }
+  }
+  #ORG {
+    background-color: #eb88b9;
+    color: #8d0649;
+    #tag {
+      width: 30px;
+      color: white;
+    }
+  }
+`;
 // const createMarkup = (text:string) => {
 //   return { __html: <div>${text}</div> };
 // };
 
 const createText = (text: string) => {
-  return <div dangerouslySetInnerHTML={{__html: text}}></div>;
+  return <div dangerouslySetInnerHTML={{ __html: text }}></div>;
 };
 
 const json = [
@@ -31,44 +59,44 @@ const json = [
     score: 0.9999986886978149,
     word: "Kano",
     start: 7,
-    end: 12
+    end: 12,
   },
   {
     entity_group: "ORG",
     score: 0.9999964237213135,
     word: "Z",
     start: 19,
-    end: 21
+    end: 21,
   },
   {
     entity_group: "PER",
     score: 0.8458274602890015,
     word: "hang",
     start: 21,
-    end: 25
+    end: 25,
   },
   {
     entity_group: "DATE",
     score: 0.9999915659427643,
     word: "18 years",
     start: 39,
-    end: 48
+    end: 48,
   },
   {
     entity_group: "LOC",
     score: 0.9999988675117493,
     word: "Nigeria",
     start: 52,
-    end: 60
-  }
+    end: 60,
+  },
 ];
 
 interface OutputProps {
-    data: Array<any>|null;
-    input: string
-  }
+  data: Array<any> | null;
+  input: string;
+}
 
-export default function Output({ data,input }: OutputProps) {
+export default function Output({ data, input }: OutputProps) {
   let tex = "Emir of Kano turban Zhang wey don spend 18 years for Nigeria";
   let arr = [...input];
 
@@ -77,12 +105,14 @@ export default function Output({ data,input }: OutputProps) {
   let word = "";
   data = json;
   let j = 0;
-  if(data!==null)
-  {
+  if (data !== null) {
     for (let i = 0; i < arr.length; i++) {
       if (j < data.length) {
         if (data[j].start === i) {
-          word +=`<span id="${data[j].entity_group}">` + data[j].word + "</span>";
+          word +=
+            ` <span id="${data[j].entity_group}">` +
+            data[j].word +
+            `<span id="tag">${data[j].entity_group}</span></span>`;
           i = data[j].end - 1;
           j += 1;
         } else {
