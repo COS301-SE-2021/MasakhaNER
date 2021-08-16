eb01ba4ae209c587da0c2916a5aad5210a140613
siphoxnkosi <u19107383@tuks.co.za>
2021-08-16 17:06:50 +0200

    Fixed link bug

--- src/components/Output/Output.tsx
+++ src/components/Output/Output.tsx
@@ -120,9 +120,9 @@ export default function Output({ data, input }: OutputProps) {
         if (data[j].start === i) {
             // localStorage.setItem('Entity',data[j].word);
           word +=
-            ` <span id="${data[j].entity.substring(2)}" onClick="${storeWord(data[j].word)}"><a href="https://www.google.com/maps/place/${data[j].word}"></a>` +
+            ` <span id="${data[j].entity.substring(2)}" onClick="${storeWord(data[j].word)}"><a href="https://www.google.com/maps/place/${data[j].word}">` +
             data[j].word +
-            `<span id="tag" onClick="${storeWord(data[j].word)}">${data[j].entity.substring(2)}</span></span>`;
+            `<span id="tag" onClick="${storeWord(data[j].word)}">${data[j].entity.substring(2)}</span></a></span>`;
           i = data[j].end - 1;
           j += 1;
         } else {
