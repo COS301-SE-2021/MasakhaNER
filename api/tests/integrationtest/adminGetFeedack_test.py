4ce0ea05e89286fdf12a573714903921379bf43d
u19081082 <u19081082@tuks.co.za>
2021-08-18 21:36:37 +0200

    admin_get_feedack test

--- api/tests/integrationtest/adminGetFeedack_test.py
+++ api/tests/integrationtest/adminGetFeedack_test.py
@@ -19,3 +19,43 @@ class Test(unittest.TestCase):
 
     def tearDown(self):
         self.main =None
+
+    def test_endpoint(self):
+        INPUT = {
+        "id":3
+        }
+
+        token = jwt.encode({'email' :'fgch@gmail.com', 'exp' : datetime.utcnow() - timedelta(minutes=60)}, app.config['SECRET_KEY'],algorithm="HS256")
+        r = self.main.admin_get_feedack('/users/1',json=INPUT,headers={'x-access-token':token})
+        data = json.loads(r.data)
+        print(data)
+        result = data['response']
+        self.assertEqual(401, r.status_code)
+        self.assertEqual(result, 'Signature has expired')
+
+    def test_endpoint2(self):
+        INPUT = {
+        "id":1
+        }
+
+        token = jwt.encode({'email' :'people@gmail.com', 'exp' : datetime.utcnow() + timedelta(minutes=60)}, app.config['SECRET_KEY'],algorithm="HS256")
+        r = self.main.admin_get_feedack('/users/1',json=INPUT,headers={'x-access-token':token})
+        data = json.loads(r.data)
+        print(data)
+        result = data['response']
+        self.assertEqual(401, r.status_code)
+        self.assertEqual(result, 'user unauthirized')
+
+    def test_endpoint3(self):
+        INPUT = {
+        "id":1
+        }
+        
+
+        token = jwt.encode({'email' :'test@test.co.za', 'exp' : datetime.utcnow() + timedelta(minutes=60)}, app.config['SECRET_KEY'],algorithm="HS256")
+        r = self.main.admin_get_feedack('/users/0',json=INPUT,headers={'x-access-token':token})
+        data = json.loads(r.data)
+        print(data)
+        result = data['response']
+        self.assertEqual(200, r.status_code)
+        self.assertEqual(result, 'deleted')
