34287be9109691d859840db5185192d54b2df854
u19081082 <u19081082@tuks.co.za>
2021-08-18 20:25:08 +0200

    reset pass test

---
+++ api/tests/integrationtest/changePassword_test.py
@@ -0,0 +1,39 @@
+import unittest
+import jwt
+import requests
+import json
+import os, sys
+from datetime import datetime, timedelta
+#sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+sys.path.append('api')
+from main import app
+
+TEST_DB = 'test.db'
+
+class BasicTests(unittest.TestCase):
+
+    main=None
+
+    def setUp(self):
+
+        app.config.from_object('config_default.Config')
+        self.main = app.test_client()
+
+    def tearDown(self):
+        pass#
+
+    def test_endpoint(self):
+        INPUT = {
+        "email": "bob@bob.com",
+        "password": "password"
+        }
+
+        token = jwt.encode({'email' :'bob@bob.com', 'exp' : datetime.utcnow() + timedelta(minutes=60)}, app.config['SECRET_KEY'],algorithm="HS256")
+        r = self.main.put('/users/299',json=INPUT,headers={'x-access-token':token})
+        data = json.loads(r.data)
+        print(data)
+
+        result = data['response']
+
+        self.assertEqual(200, r.status_code)
+        self.assertEqual(result, 'Signature has expired')
