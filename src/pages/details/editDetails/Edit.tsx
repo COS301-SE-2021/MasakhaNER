b206d52c0c6c5edf2d18507222f2c2a0f2cc8cce
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2021-08-18 21:37:01 +0200

    Added button components

--- src/pages/details/editDetails/Edit.tsx
+++ src/pages/details/editDetails/Edit.tsx
@@ -49,6 +49,23 @@ const Input = styled.input`
   }
 `;
 
+const Button = styled.button`
+  border: solid 1px rgba(0, 0, 0, 0.2);
+  margin-bottom: 20px;
+  width: 10em;
+  background-color: white;
+  border-radius: 20px;
+  height: 35px;
+  width: 15em;
+  box-shadow: 0px 2px 3px 0px rgba(0, 0, 0, 0.1);
+  &:hover {
+    border: solid 1px rgba(0, 0, 0, 0.2);
+    border-radius: 20px;
+    box-shadow: 0px 2px 3px 0px rgba(0, 0, 0, 0.3);
+    transition: 0.4s;
+  }
+`;
+
 function Register() {
   const [firstName, setfirstName] = useState("");
   const [lastName, setlastName] = useState("");
