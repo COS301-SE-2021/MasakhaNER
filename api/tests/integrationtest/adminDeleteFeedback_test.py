de216d684e36bdc0f4af4cd6947f14fe3cf53644
u19081082 <u19081082@tuks.co.za>
2021-08-18 20:38:45 +0200

    admin delete feedback

--- api/tests/integrationtest/adminDeleteFeedback_test.py
+++ api/tests/integrationtest/adminDeleteFeedback_test.py
@@ -3,3 +3,7 @@ import requests
 import json
 import jwt
 from datetime import datetime, timedelta
+#sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+import os, sys
+sys.path.append('api')
+from main import app
