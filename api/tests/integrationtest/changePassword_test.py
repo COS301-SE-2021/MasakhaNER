f3daeeb6ae034aa368e82c146297406f88ac0353
u19081082 <u19081082@tuks.co.za>
2021-08-18 08:23:39 +0200

    changePass test

---
+++ api/tests/integrationtest/changePassword_test.py
@@ -0,0 +1,7 @@
+import unittest
+import requests
+import json
+import os, sys
+#sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+sys.path.append('api')
+from main import app
