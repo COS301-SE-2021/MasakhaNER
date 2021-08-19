5e9f94f10aa5b2a8ec70f853dc4f97019854f215
Khotso-Bore <u19180642@tuks.co.za>
2021-08-19 19:03:18 +0200

    save more

--- api/tests/integrationtest/adminGetAllFeedback_test.py
+++ api/tests/integrationtest/adminGetAllFeedback_test.py
@@ -1,5 +1,6 @@
 import unittest
 import requests
+import bcrypt
 import json
 import jwt
 from datetime import datetime, timedelta
@@ -15,44 +16,27 @@ class Test(unittest.TestCase):
     def setUp(self):
 
         app.config.from_object('config_default.Config')
+        db = app.config['DATABASE']
+        encoded_password = bytes("password", encoding='utf-8')
+        encrypted_password = bcrypt.hashpw(
+        encoded_password, bcrypt.gensalt())
+        #print(type(encrypted_password))
+        encrypted_password = encrypted_password.decode('UTF-8')
+        sql = "INSERT INTO users (id,firstname,lastname,password,email,isadmin,activationcode, verified) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)"
+        db.cur.execute(sql,(0,'integeration', 'test',encrypted_password,'admin@test.com',True,000,True))
+        db.conn.commit()
         self.main = app.test_client()
 
     def tearDown(self):
+        db = app.config['DATABASE']
+        sql = "DELETE FROM users WHERE id =%s"
+        db.cur.execute(sql,(0,))
         self.main =None
 
-    # def test_endpoint(self):
-    #     INPUT = {
-    #     "id":3
-    #     }
-
-    #     token = jwt.encode({'email' :'fgch@gmail.com', 'exp' : datetime.utcnow() - timedelta(minutes=60)}, app.config['SECRET_KEY'],algorithm="HS256")
-    #     r = self.main.get('/users/1',json=INPUT,headers={'x-access-token':token})
-    #     data = json.loads(r.data)
-    #     print(data)
-    #     result = data['response']
-    #     self.assertEqual(401, r.status_code)
-    #     self.assertEqual(result, 'Signature has expired')
-
-    # def test_endpoint2(self):
-    #     INPUT = {
-    #     "id":1
-    #     }
-
-    #     token = jwt.encode({'email' :'people@gmail.com', 'exp' : datetime.utcnow() + timedelta(minutes=60)}, app.config['SECRET_KEY'],algorithm="HS256")
-    #     r = self.main.get('/users/1',json=INPUT,headers={'x-access-token':token})
-    #     data = json.loads(r.data)
-    #     print(data)
-    #     result = data['response']
-    #     self.assertEqual(401, r.status_code)
-    #     self.assertEqual(result, 'user unauthirized')
-
     def test_endpoint3(self):
-        INPUT = {
-        "id":1
-        }
         
         token = jwt.encode({'email' :'test@test.co.za', 'exp' : datetime.utcnow() + timedelta(minutes=60)}, app.config['SECRET_KEY'],algorithm="HS256")
-        r = self.main.get('/feedback/0',json=INPUT,headers={'x-access-token':token})
+        r = self.main.get('/feedback',headers={'x-access-token':token})
         data = json.loads(r.data)
         print(data)
         self.assertEqual(200, r.status_code)
