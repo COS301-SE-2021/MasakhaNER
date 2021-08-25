dda7ecf61b3d9719dab8b280d5c9efb8f14f926e
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2021-08-25 21:16:28 +0200

    Added visualizer page and button on dashboard

---
+++ src/pages/visualizer/Visualizer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useHistory } from "react-router-dom";
+import Nav from "../../components/nav/Nav";
+
+let history;
+
+const Visualizer = () => {
+  return <></>;
+};
+
+export default Visualizer;
