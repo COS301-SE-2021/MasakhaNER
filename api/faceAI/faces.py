67e1d7d9c4038c62e4a45f6dbe6549a4cd80d999
Khotso-Bore <u19180642@tuks.co.za>
2021-08-28 19:37:47 +0200

    grayscaling added

--- api/faceAI/faces.py
+++ api/faceAI/faces.py
@@ -8,7 +8,14 @@ cap = cv2.VideoCapture(0)
 while(True):
     #capture frams
     ret,frame = cap.read()
-    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces  = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for(x,y,w,h) in faces:
+        print(x,y,w,h)
+        roi_gray = gray[y:y+h,x:x+w]
+        img_item = "my-image.png"
+        cv2.imwrite(img_item, roi_gray)
+
     #Display frame
     cv2.imshow('frame',frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
