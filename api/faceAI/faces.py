367838e5482d9a110d267ea9a61f0c2391f4a98a
Khotso-Bore <u19180642@tuks.co.za>
2021-08-28 21:25:55 +0200

    faces for testing

--- api/faceAI/faces.py
+++ api/faceAI/faces.py
@@ -8,28 +8,32 @@ recogniser = cv2.face.LBPHFaceRecognizer_create()
 recogniser.read("trainner.yml")
 
 labels = {}
-with open("labels.pickel", "rb") as f:
+with open("labels.pickle", "rb") as f:
     flabels = pickle.load(f)
-    labels = {v:k for k,v in flabels.items}
+    labels = {v:k for k,v in flabels.items()}
 
-cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture(0)
+cap = cv2.imread("api/faceAI/pte.jpg",0)
+print(type(cap),cap)
 
 while(True):
     #capture frams
-    ret,frame = cap.read()
+    #ret,frame = cap.read()
+    frame = cap
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces  = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
     for(x,y,w,h) in faces:
-        print(x,y,w,h)
+        #print(x,y,w,h)
         roi_gray = gray[y:y+h,x:x+w]
         roi_color = frame[y:y+h,x:x+w]
         
 
         id_, conf = recogniser.predict(roi_gray)
-        if conf>=45 and conf <= 85:
-            print(id_)
-            print(labels[id_])
+        if conf>=55:
+            #print(id_)
+            #print(labels[id_])
             font = cv2.FONT_HERSHEY_COMPLEX
+            name = labels[id_]
             color = (255,255,255)
             stroke = 2
             cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
