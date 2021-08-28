535a93073350d1e4c4c4362153928f4882ea1a61
Khotso-Bore <u19180642@tuks.co.za>
2021-08-28 20:50:40 +0200

    resise images

--- api/faceAI/face-train.py
+++ api/faceAI/face-train.py
@@ -31,6 +31,8 @@ for root, dirs, files in os.walk(image_dir):
 
                 #convert image to array
                 pil_image = Image.open(path).convert("L")
+                size = (550,550)
+                pil_image = pil_image.resize(size, Image.ANTIALIAS)
                 image_array = np.array(pil_image, "uint8")
 
                 faces  = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
