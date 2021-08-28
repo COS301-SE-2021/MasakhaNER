7908351ff8316f23e159ffd478f444de1d20681d
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2021-08-28 20:13:55 +0200

    Created word count towers for visualizer

--- src/pages/visualizer/Visualizer.tsx
+++ src/pages/visualizer/Visualizer.tsx
@@ -2,24 +2,29 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Canvas } from "@react-three/fiber";
 import { Physics, usePlane, useBox } from "@react-three/cannon";
-import { OrbitControls, Stars } from "@react-three/drei";
+import { OrbitControls, Stars, Sky } from "@react-three/drei";
 import "./Visualizer.css";
 interface Props {
   height: number;
+  position_x: number;
+  position_z: number;
 }
 
-const Box: React.FC<Props> = ({height}) => {
-  const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
+const Box: React.FC<Props> = ({ height, position_x, position_z }) => {
+  const [ref, api] = useBox(() => ({
+    mass: 0.1,
+    position: [position_x, 1, position_z],
+  }));
   return (
     <mesh
       onClick={() => {
-        api.velocity.set(0, 2, 0);
+        api.velocity.set(0, 0, 0);
       }}
       ref={ref}
-      position={[3000, 2, 0]}
+      position={[0, 0, 0]}
     >
       <boxGeometry args={[1, height, 1]} />
-      <meshLambertMaterial attach="material" color="lightblue" />
+      <meshLambertMaterial attach="material" color="green" />
     </mesh>
   );
 };
@@ -29,9 +34,9 @@ const Plane = (props: JSX.IntrinsicElements["mesh"]) => {
     rotation: [-Math.PI / 2, 0, 0],
   }));
   return (
-    <mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]} >
+    <mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]}>
       <planeBufferGeometry attach="geometry" args={[100, 100]} />
-      <meshLambertMaterial attach="material" color="white" />
+      <meshLambertMaterial attach="material" color="lightblue" />
     </mesh>
   );
 };
@@ -43,10 +48,20 @@ const Visualizer = () => {
         <OrbitControls />
         <Stars />
         <ambientLight intensity={0.4} />
-        <spotLight position={[10, 15, 10]} angle={0.3} />
+        <spotLight position={[10, 15, 10]} angle={0.3} castShadow />
         <Physics>
-          <Box height={1} />
-          <Box height={1} />
+          <Box height={1} position_x={-4} position_z={0} />
+          <Box height={4} position_x={-2.5} position_z={0} />
+          <Box height={9} position_x={-1} position_z={0} />
+          <Box height={2} position_x={0.5} position_z={0} />
+          <Box height={1} position_x={2} position_z={0} />
+          <Box height={7} position_x={3.5} position_z={0} />
+          <Box height={3} position_x={-4} position_z={-1.5} />
+          <Box height={1} position_x={-2.5} position_z={-1.5} />
+          <Box height={5} position_x={-1} position_z={-1.5} />
+          <Box height={1} position_x={0.5} position_z={-1.5} />
+          <Box height={4} position_x={2} position_z={-1.5} />
+          <Box height={3} position_x={3.5} position_z={-1.5} />
           <Plane />
         </Physics>
       </Canvas>
