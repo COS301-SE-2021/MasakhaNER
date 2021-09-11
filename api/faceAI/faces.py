ed03d1f44a81a97141129f3051aede953c74b99d
Khotso-Bore <u19180642@tuks.co.za>
2021-09-11 13:28:16 +0200

    save diss

--- api/faceAI/faces.py
+++ api/faceAI/faces.py
@@ -4,7 +4,8 @@ import cv2
 import pickle
 
 dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, "cascades/data/haarcascade_frontalface_alt2.xml")
+filename = os.path.join(
+    dirname, "cascades/data/haarcascade_frontalface_default.xml")
 face_cascade = cv2.CascadeClassifier(filename)
 
 filename = os.path.join(dirname, "trainner.yml")
@@ -15,16 +16,16 @@ labels = {}
 filename = os.path.join(dirname, "labels.pickle")
 with open(filename, "rb") as f:
     flabels = pickle.load(f)
-    labels = {v:k for k,v in flabels.items()}
+    labels = {v: k for k, v in flabels.items()}
 
-filename = os.path.join(dirname, "kit.jpg")
+filename = os.path.join(dirname, "58.jpg")
 cap = cv2.VideoCapture(0)
 image = cv2.imread(filename)
 
-#while(True):
-#capture frams
+# while(True):
+# capture frams
 frame = image
-#print(frame,type(frame))
+# print(frame,type(frame))
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 #faces  = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5,minSize=(60, 60),flags=cv2.CASCADE_SCALE_IMAGE)
 faces = face_cascade.detectMultiScale(
@@ -32,40 +33,46 @@ faces = face_cascade.detectMultiScale(
     scaleFactor=1.1,
     minNeighbors=5,
     minSize=(30, 30),
-    flags = cv2.CASCADE_SCALE_IMAGE
+    flags=cv2.CASCADE_SCALE_IMAGE
 )
 print(faces)
-for(x,y,w,h) in faces:
-    #print(x,y,w,h)
-    roi_gray = gray[y:y+h,x:x+w]
-    roi_color = frame[y:y+h,x:x+w]
-    
+for(x, y, w, h) in faces:
+    # print(x,y,w,h)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = frame[y:y+h, x:x+w]
 
     id_, conf = recogniser.predict(roi_gray)
-    print(id_)
-    print(labels[id_])
-    font = cv2.FONT_HERSHEY_COMPLEX
-    name = labels[id_]
-    color = (255,255,255)
-    stroke = 2
-    cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+    print(conf)
+    if conf < 51:
+        # print(id_)
+        # print(labels[id_])
 
-    img_item = "my-image.png"
-    cv2.imwrite(img_item, roi_gray)
-    
-    #draw rectangle
-    color = (0,255,0)
-    stroke = 2
-    width = x+w
-    height = y+h
-    cv2.rectangle(frame, (x,y), (width,height), color, stroke)
+        font = cv2.FONT_HERSHEY_COMPLEX
+        name = labels[id_]
+        color = (255, 255, 255)
+        stroke = 2
+        cv2.putText(frame, name, (x, y), font, 1, color, stroke, cv2.LINE_AA)
+    elif(conf < 100):
+        font = cv2.FONT_HERSHEY_COMPLEX
+        name = "Unkwon"
+        color = (255, 255, 255)
+        stroke = 2
+        cv2.putText(frame, name, (x, y), font, 1, color, stroke, cv2.LINE_AA)
+
+    if conf < 100:
+        img_item = "my-image.png"
+        cv2.imwrite(img_item, roi_gray)
+
+        # draw rectangle
+        color = (0, 255, 0)
+        stroke = 2
+        width = x+w
+        height = y+h
+        cv2.rectangle(frame, (x, y), (width, height), color, stroke)
 
 
-#Display frame
+# Display frame
 
-cv2.imshow('frame',frame)
+cv2.imwrite('theframe.jpg', frame)
+cv2.imshow('frames', frame)
 cv2.waitKey(0)
-
-#release capture
-# cap.release()
-# cv2.destroyAllWindows()
