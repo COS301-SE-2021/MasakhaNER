6439f0784ee0e10de13d73a365de0c532558b7bd
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2021-09-15 13:10:38 +0200

    design changes

--- src/pages/verify/Verify.tsx
+++ src/pages/verify/Verify.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./verifyAccount.css"
-import { Link } from 'react-router-dom';
+import "./verifyAccount.css";
+import { Link } from "react-router-dom";
 
 function VerifyAccount() {
   const [code, setCode] = useState("");
@@ -13,8 +13,8 @@ function VerifyAccount() {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      "email": localStorage.getItem("newEmail"),
-      "code": code
+      email: localStorage.getItem("newEmail"),
+      code: code,
     }),
   };
 
@@ -26,22 +26,20 @@ function VerifyAccount() {
     try {
       const resp = await fetch("/verify", options);
       console.log("This is what came back: ", options);
-  
+
       if (resp.status === 200) {
         const data = await resp.json();
         alert("You have successfully verified your account!");
-        window.location.href = "/";
-      }
-      else {
+        window.location.href = "/Dashboard";
+      } else {
         alert("Incorrect verification code!");
         window.location.href = "/verify";
       }
-    }
-    catch (error) {
+    } catch (error) {
       console.log("there is an error", error);
       window.location.href = "/verify";
     }
-  }
+  };
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
@@ -51,7 +49,10 @@ function VerifyAccount() {
     <div className="veryify-form">
       <form onSubmit={handleSubmit}>
         <h3>Enter Confirmation Code</h3>
-        <p>Enter the confirmation code we sent to your email.<em>{localStorage.getItem("newEmail")}</em></p>
+        <p>
+          Enter the confirmation code we sent to your email.
+          <em>{localStorage.getItem("newEmail")}</em>
+        </p>
         <div className="form-group">
           <input
             type="text"
@@ -73,11 +74,14 @@ function VerifyAccount() {
               e.preventDefault();
               handleVerify();
               //setClicked(!clicked);
-            }}>Next</button>
+            }}
+          >
+            Next
+          </button>
         </Link>
       </form>
     </div>
-  )
+  );
 }
 
-export default VerifyAccount
+export default VerifyAccount;
