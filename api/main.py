06d286e1658d8e7d376e26a0b8ad7e8d2db3f28f
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2021-09-18 11:28:42 +0200

    Update main.py

--- api/main.py
+++ api/main.py
@@ -17,7 +17,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 app = Flask(__name__)
 app.config.from_object('config_default.Config')
 
-runModel("load")
+# runModel("load")
 
 # app.config['SECRET_KEY']='secret'
 # app.config['DATABASE']=User()
