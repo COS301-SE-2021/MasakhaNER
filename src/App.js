3b713424b898a8a1af0d7582b2ee28c1f017805b
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2021-09-19 04:19:52 +0200

    kgvj

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ import Admin from "./pages/admin/AdminUser";
 import Edit from "./pages/details/editDetails/Edit";
 import Changepass from "./pages/details/resetPassword/Changepass";
 import About from "./pages/about/About";
-// import Visualizer from "./pages/visualizer/Visualizer";
 
 function App() {
   return (
@@ -24,17 +23,13 @@ function App() {
           {/* <Redirect from="/Admin#/logins" to="/" /> */}
           <Route exact path="/" component={Login} />
           {/* <Route exact path="/register" component={Register} /> */}
-          <Route exact path="/dashboard" component={Dashboard} />
+          {/* <Route exact path="/dashboard" component={Dashboard} /> */}
           {/* <Route exact path="/visualizer" component={Visualizer} /> */}
           {/* <Route exact path="/verify" component={Verify} /> */}
           {/* <Route exact path="/admin" component={Admin} /> */}
           {/* <Route exact path="/details/edit" component={Edit} /> */}
           {/* <Route exact path="/details/changepassword" component={Changepass} /> */}
           {/* <Route exact path="/about" component={About} /> */}
-<<<<<<< HEAD
-=======
-          {/* <Route exact path="/visualizer" component={Visualizer} /> */}
->>>>>>> parent of e1095dc06 (added visualizer)
         </Switch>
       </Router>
     </div>
