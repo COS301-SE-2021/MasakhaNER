32af52e2c2f30aa2ed4731fd596814c7bb1f425f
Khotso-Bore <u19180642@tuks.co.za>
2021-10-07 00:56:54 +0200

    updated req

--- api/translator/TokenizerWrap.py
+++ api/translator/TokenizerWrap.py
@@ -10,10 +10,8 @@ import math
 import IPython
 import pickle
 import IPython
-import pandas as pd
 import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
 
 
 class TokenizerWrap(Tokenizer):
--- api/translator/trans2.py
+++ api/translator/trans2.py
@@ -10,10 +10,8 @@ import math
 import IPython
 import pickle
 import IPython
-import pandas as pd
 import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
 #from TokenizerWrap import TokenizerWrap
 #get_ipython().run_line_magic('matplotlib', 'inline')
 
