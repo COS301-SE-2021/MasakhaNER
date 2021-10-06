fc1efc21637bdd91fa396f486e9ed30310d14cbe
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2021-10-06 14:16:56 +0200

    Update model.py

--- api/model.py
+++ api/model.py
@@ -1,18 +1,4 @@
-# from transformers import AutoTokenizer, AutoModelForTokenClassification
-# from transformers import pipeline
-
-
-# def runModel(input):
-#     url = "Davlan/bert-base-multilingual-cased-masakhaner"
-#     tokenizer = AutoTokenizer.from_pretrained(url)
-#     model = AutoModelForTokenClassification.from_pretrained(url)
-#     nlp = pipeline("ner", model=model, tokenizer=tokenizer)
-#     example = "Emir of Kano turban Zhang wey don spend 18 years for Nigeria"
-#     ner_results = nlp(input)
-#     return ner_results
-
 import json
-
 import requests
 
 API_URL = "https://api-inference.huggingface.co/models/Davlan/bert-base-multilingual-cased-masakhaner"
@@ -31,9 +17,3 @@ def runModel(input):
     })
 
     return data
-
-
-# output = runModel(
-#     "Emir of Kano turban Zhang wey don spend 18 years for Nigeria")
-
-# print(output)
